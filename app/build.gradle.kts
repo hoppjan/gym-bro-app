0ffc8d131fc548521e7f9cb6e5f6d55db745663f
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-06-09 22:10:16 +0200

    Add koin compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(project.dependencies.platform(libs.koin.bom))
     implementation(libs.koin.core)
     implementation(libs.koin.android)
+    implementation(libs.koin.androidx.compose)
     implementation(libs.sqldelight.android.driver)
     implementation(libs.sqldelight.coroutines)
     testImplementation(libs.junit)
