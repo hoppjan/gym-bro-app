ca545b618fe02637e7277c9b1e7bc6511de68e42
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-06-12 22:47:19 +0200

    Improve routine screen header with emoji

--- app/src/main/java/de/janhopp/gymbro/ui/workout/routine/WorkoutRoutineScreen.kt
+++ app/src/main/java/de/janhopp/gymbro/ui/workout/routine/WorkoutRoutineScreen.kt
@@ -1,12 +1,14 @@
 package de.janhopp.gymbro.ui.workout.routine
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import de.janhopp.gymbro.model.workout.WorkoutRoutine
 
 @Composable
@@ -17,15 +19,28 @@ fun WorkoutRoutineScreen(
     Column(
         modifier = modifier,
     ) {
+        routine.emoji?.let {
+            Text(
+                text = it,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 48.dp),
+                style = MaterialTheme.typography.displayLarge,
+            )
+        }
         Text(
             text = routine.name,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(top = 32.dp, bottom = 8.dp),
             style = MaterialTheme.typography.titleLarge,
         )
         routine.description?.let { description ->
             Text(
                 text = description,
-                modifier = Modifier.align(Alignment.CenterHorizontally),
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(bottom = 8.dp),
                 style = MaterialTheme.typography.bodyMedium,
             )
         }
