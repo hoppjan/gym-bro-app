b0952cef8499ec3740da9e9be31ac15139a6ccd0
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-06-12 22:16:50 +0200

    Improve code of bad ui

--- app/src/main/java/de/janhopp/gymbro/MainActivity.kt
+++ app/src/main/java/de/janhopp/gymbro/MainActivity.kt
@@ -1,15 +1,12 @@
 package de.janhopp.gymbro
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,7 +15,6 @@ import de.janhopp.gymbro.ui.theme.GymBroTheme
 import de.janhopp.gymbro.ui.workout.WorkoutRoutinePickerScreen
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -30,31 +26,27 @@ class MainActivity : ComponentActivity() {
         setContent {
             GymBroTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding),
-                    ) {
-                        WorkoutRoutinePickerScreen(routines)
-                    }
+                    WorkoutRoutinePickerScreen(
+                        modifier = Modifier.padding(innerPadding),
+                        workoutRoutines = routines,
+                    )
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GymBroTheme {
-        Greeting("Android")
+        WorkoutRoutinePickerScreen(
+            modifier = Modifier.fillMaxSize(),
+            workoutRoutines = listOf(
+                WorkoutRoutine(0, "Beine", "Unterkörper", emptyList()),
+                WorkoutRoutine(0, "Arme", "Oberkörper", emptyList()),
+                WorkoutRoutine(0, "Po", "Popo", emptyList()),
+            ),
+        )
     }
 }
--- app/src/main/java/de/janhopp/gymbro/ui/workout/WorkoutRoutinePickerScreen.kt
+++ app/src/main/java/de/janhopp/gymbro/ui/workout/WorkoutRoutinePickerScreen.kt
@@ -11,25 +11,28 @@ import de.janhopp.gymbro.model.workout.WorkoutRoutine
 
 @Composable
 fun WorkoutRoutinePickerScreen(
+    modifier: Modifier = Modifier,
     workoutRoutines: List<WorkoutRoutine>,
 ) {
-    Text(
-        text = "Workout Routines",
-        style = MaterialTheme.typography.titleLarge,
-        modifier = Modifier.padding(vertical = 16.dp),
-    )
-    workoutRoutines.forEach { routine ->
-        Column(
-            modifier = Modifier.padding(vertical = 4.dp),
-        ) {
-            Text(
-                text = routine.name,
-                style = MaterialTheme.typography.titleMedium,
-            )
-            Text(
-                routine.description ?: "",
-                style = MaterialTheme.typography.bodyMedium,
-            )
+    Column(modifier = modifier) {
+        Text(
+            text = "Workout Routines",
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(vertical = 16.dp),
+        )
+        workoutRoutines.forEach { routine ->
+            Column(
+                modifier = Modifier.padding(vertical = 4.dp),
+            ) {
+                Text(
+                    text = routine.name,
+                    style = MaterialTheme.typography.titleMedium,
+                )
+                Text(
+                    routine.description ?: "",
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+            }
         }
     }
 }
