7c391967dc725d004383b808463053ff493e9619
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-06-13 17:48:31 +0200

    Make column scrollable

--- app/src/main/java/de/janhopp/gymbro/ui/workout/WorkoutRoutinePickerScreen.kt
+++ app/src/main/java/de/janhopp/gymbro/ui/workout/WorkoutRoutinePickerScreen.kt
@@ -1,13 +1,12 @@
 package de.janhopp.gymbro.ui.workout
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ListItem
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import de.janhopp.gymbro.model.workout.WorkoutRoutine
 
 @Composable
@@ -15,12 +14,9 @@ fun WorkoutRoutinePickerScreen(
     modifier: Modifier = Modifier,
     workoutRoutines: List<WorkoutRoutine>,
 ) {
-    Column(modifier = modifier) {
-        Text(
-            text = "Workout Routines",
-            style = MaterialTheme.typography.titleLarge,
-            modifier = Modifier.padding(all = 16.dp),
-        )
+    Column(
+        modifier = modifier.verticalScroll(rememberScrollState()),
+    ) {
         workoutRoutines.forEach { routine ->
             ListItem(
                 headlineContent = {
