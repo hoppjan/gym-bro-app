4eaeed411f3905e9e7f986fe59237ea758fc5b50
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-06-13 17:43:26 +0200

    Set fresh TopAppBar

--- app/src/main/java/de/janhopp/gymbro/MainActivity.kt
+++ app/src/main/java/de/janhopp/gymbro/MainActivity.kt
@@ -6,7 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,7 +25,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GymBroTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        @OptIn(ExperimentalMaterial3Api::class)
+                        TopAppBar(
+                            title = { Text("Gym Bro") },
+                        )
+                    },
+                ) { innerPadding ->
                     WorkoutRoutinePickerScreen(
                         modifier = Modifier.padding(innerPadding),
                         workoutRoutines = routines,
