63e408110b1b75a9025952c1edc5bb2b6c8b2f51
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-06-22 15:38:14 +0200

    Refactor Set to match new model

--- app/src/main/java/de/janhopp/gymbro/model/execution/Set.kt
+++ app/src/main/java/de/janhopp/gymbro/model/execution/Set.kt
@@ -1,6 +1,25 @@
 package de.janhopp.gymbro.model.execution
 
-data class Set(
-    val id: Long,
-    val index: Int,
-)
+interface Set {
+    val id: Long
+    val index: Int
+}
+
+data class WeightSet(
+    override val id: Long,
+    override val index: Int,
+    val weight: Int,
+    val reps: Int,
+) : Set
+
+data class BodyWeightSet(
+    override val id: Long,
+    override val index: Int,
+    val reps: Int,
+) : Set
+
+data class CardioSet(
+    override val id: Long,
+    override val index: Int,
+    val duration: Int,
+) : Set
