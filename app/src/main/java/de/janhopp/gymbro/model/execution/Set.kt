6fe99c24f94d5ee3ea41b1bf2d56365fc7f168c3
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-06-22 15:46:30 +0200

    Add conversion functions

--- app/src/main/java/de/janhopp/gymbro/model/execution/Set.kt
+++ app/src/main/java/de/janhopp/gymbro/model/execution/Set.kt
@@ -1,5 +1,7 @@
 package de.janhopp.gymbro.model.execution
 
+import de.janhopp.gymbro.db.model.ExerciseType
+import de.janhopp.gymbro.db.model.execution.SetTable
 import de.janhopp.gymbro.model.exercise.Weight
 
 sealed interface Set {
@@ -25,3 +27,19 @@ data class CardioSet(
     override val index: Int,
     val duration: Int,
 ) : Set
+
+fun SetTable.toWeightSet() = WeightSet(id, index, weight!!, reps!!)
+fun SetTable.toBodyWeightSet() = BodyWeightSet(id, index, reps!!)
+fun SetTable.toCardioSet() = CardioSet(id, index, duration!!)
+
+fun SetTable.toSet(type: ExerciseType) = when (type) {
+    ExerciseType.WEIGHT -> toWeightSet()
+    ExerciseType.BODY_WEIGHT -> toBodyWeightSet()
+    ExerciseType.CARDIO -> toCardioSet()
+}
+
+fun Set.toSetTable(exerciseExecutionId: Int) = when (this) {
+    is WeightSet -> SetTable(id, index, exerciseExecutionId, reps = reps, weight = weight, duration = null)
+    is BodyWeightSet -> SetTable(id, index, exerciseExecutionId, reps = reps, weight = null, duration = null)
+    is CardioSet -> SetTable(id, index, exerciseExecutionId, null, null, duration)
+}
