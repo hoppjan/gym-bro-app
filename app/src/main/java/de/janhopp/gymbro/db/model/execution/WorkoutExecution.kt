afe60a12089c7120335eff2f27a29860033c7133
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-06-22 14:40:55 +0200

    Add WorkoutExecution foreign key to WorkoutPlanTable

--- app/src/main/java/de/janhopp/gymbro/db/model/execution/WorkoutExecution.kt
+++ app/src/main/java/de/janhopp/gymbro/db/model/execution/WorkoutExecution.kt
@@ -1,11 +1,18 @@
 package de.janhopp.gymbro.db.model.execution
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import de.janhopp.gymbro.db.model.planning.WorkoutPlanTable
 
-@Entity
+@Entity(
+    foreignKeys = [
+        ForeignKey(entity = WorkoutPlanTable::class, parentColumns = ["id"], childColumns = ["workoutPlanId"]),
+    ]
+)
 data class WorkoutExecution(
     @PrimaryKey(autoGenerate = true) val id: Int,
     val start: Long,
     val finish: Long,
+    val workoutPlanId: Int,
 )
