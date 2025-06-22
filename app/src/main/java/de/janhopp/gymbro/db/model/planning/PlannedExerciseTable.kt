afd3a5c6731cca1cfa73aabb981865930b0d0f52
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-06-22 14:28:59 +0200

    Add missing id

--- app/src/main/java/de/janhopp/gymbro/db/model/planning/PlannedExerciseTable.kt
+++ app/src/main/java/de/janhopp/gymbro/db/model/planning/PlannedExerciseTable.kt
@@ -1,9 +1,11 @@
 package de.janhopp.gymbro.db.model.planning
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
 data class PlannedExerciseTable(
+    @PrimaryKey(autoGenerate = true) val id: Int,
     val name: String,
     val index: Int,
     val pauseInBetween: Int,
