72da5b1593908ef542f3c5099c495885bf5288c6
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-06-22 14:42:46 +0200

    Refactor relation from WorkoutPlanTable to PersonTable

--- app/src/main/java/de/janhopp/gymbro/db/model/planning/WorkoutPlanTable.kt
+++ app/src/main/java/de/janhopp/gymbro/db/model/planning/WorkoutPlanTable.kt
@@ -1,12 +1,19 @@
 package de.janhopp.gymbro.db.model.planning
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import de.janhopp.gymbro.db.model.PersonTable
 
-@Entity
+@Entity(
+    foreignKeys = [
+        ForeignKey(entity = PersonTable::class, parentColumns = ["id"], childColumns = ["personId"]),
+    ]
+)
 data class WorkoutPlanTable(
     @PrimaryKey(autoGenerate = true) val id: Int,
     val name: String,
     val description: String?,
     val icon: String?,
+    val personId: Int,
 )
