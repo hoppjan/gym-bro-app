9a4c379b70a226c4b9e2a9839b7d41d72e55fc21
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-06-22 15:35:59 +0200

    Refactor ExerciseExecution to match new model

--- app/src/main/java/de/janhopp/gymbro/model/execution/ExerciseExecution.kt
+++ app/src/main/java/de/janhopp/gymbro/model/execution/ExerciseExecution.kt
@@ -3,6 +3,8 @@ package de.janhopp.gymbro.model.execution
 import de.janhopp.gymbro.model.planning.PlannedExercise
 
 data class ExerciseExecution(
+    val id: Int,
+    val index: Int,
     val exercise: PlannedExercise,
     val sets: List<Set>,
 )
