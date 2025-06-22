ffba705e99439e8afd609a12f840cf592fda7b24
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-06-22 16:19:51 +0200

    Add dao injection

--- app/src/main/java/de/janhopp/gymbro/GymBroApp.kt
+++ app/src/main/java/de/janhopp/gymbro/GymBroApp.kt
@@ -3,6 +3,12 @@ package de.janhopp.gymbro
 import android.app.Application
 import androidx.room.Room
 import de.janhopp.gymbro.db.GymDatabase
+import de.janhopp.gymbro.db.daos.ExerciseExecutionDao
+import de.janhopp.gymbro.db.daos.PersonDao
+import de.janhopp.gymbro.db.daos.PlannedExerciseDao
+import de.janhopp.gymbro.db.daos.SetDao
+import de.janhopp.gymbro.db.daos.WorkoutExecutionDao
+import de.janhopp.gymbro.db.daos.WorkoutPlanDao
 import de.janhopp.gymbro.ui.workout.routine.WorkoutRoutineViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -22,6 +28,12 @@ class GymBroApp : Application() {
             modules(
                 module {
                     single<GymDatabase> { db }
+                    single<PersonDao> { db.personDao() }
+                    single<WorkoutPlanDao> { db.workoutPlanDao() }
+                    single<PlannedExerciseDao> { db.plannedExerciseDao() }
+                    single<WorkoutExecutionDao> { db.workoutExecutionDao() }
+                    single<ExerciseExecutionDao> { db.exerciseExecutionDao() }
+                    single<SetDao> { db.setDao() }
                     viewModel { WorkoutRoutineViewModel() }
                 },
             )
