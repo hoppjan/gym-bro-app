46542a23f4c84b148aa49e93b8007baec809000d
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-06-22 13:34:57 +0200

    Add WorkoutPlanTable to have separate db model

---
+++ app/src/main/java/de/janhopp/gymbro/db/model/planning/WorkoutPlanTable.kt
@@ -0,0 +1,12 @@
+package de.janhopp.gymbro.db.model.planning
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class WorkoutPlanTable(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val name: String,
+    val description: String?,
+    val emoji: String?,
+)
