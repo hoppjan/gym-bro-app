7f0149cf8abbabeb5ec9996a779cbff74096ebbb
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-06-22 17:30:16 +0200

    Add birthday to Person

--- app/src/main/java/de/janhopp/gymbro/model/Person.kt
+++ app/src/main/java/de/janhopp/gymbro/model/Person.kt
@@ -1,9 +1,11 @@
 package de.janhopp.gymbro.model
 
 import de.janhopp.gymbro.model.planning.WorkoutPlan
+import java.util.Date
 
 data class Person(
     val id: Int,
     val name: String,
+    val birthday: Date,
     val workoutPlans: List<WorkoutPlan> = emptyList(),
 )
