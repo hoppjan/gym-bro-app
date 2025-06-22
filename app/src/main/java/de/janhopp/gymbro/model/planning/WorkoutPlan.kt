ce80b53162795f7fcd9ad318073ee63cb0ec9585
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-06-22 15:13:34 +0200

    Update WorkoutPlan to be like WorkoutPlanTable

--- app/src/main/java/de/janhopp/gymbro/model/planning/WorkoutPlan.kt
+++ app/src/main/java/de/janhopp/gymbro/model/planning/WorkoutPlan.kt
@@ -1,13 +1,8 @@
 package de.janhopp.gymbro.model.planning
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity
 data class WorkoutPlan(
-    @PrimaryKey(autoGenerate = true) val id: Int,
+    val id: Int,
     val name: String,
     val description: String?,
-    val emoji: String?,
-    //val exercises: List<Exercise> = emptyList(),
+    val icon: String?,
 )
