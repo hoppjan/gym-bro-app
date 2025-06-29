25d88156860879e54a0ba2f897cc2a5651de44d4
hoppjan <83405332+hoppjan@users.noreply.github.com>
2025-06-29 20:57:31 +0200

    Add model mapper functions

--- app/src/main/java/de/janhopp/gymbro/model/planning/WorkoutPlan.kt
+++ app/src/main/java/de/janhopp/gymbro/model/planning/WorkoutPlan.kt
@@ -1,5 +1,7 @@
 package de.janhopp.gymbro.model.planning
 
+import de.janhopp.gymbro.db.model.planning.WorkoutPlanTable
+
 data class WorkoutPlan(
     val id: Int,
     val name: String,
@@ -7,3 +9,10 @@ data class WorkoutPlan(
     val icon: String?,
     val exercises: List<PlannedExercise> = emptyList(),
 )
+
+// TODO: add planned exercises
+fun WorkoutPlanTable.toWorkoutPlan() =
+    WorkoutPlan(id, name, description, icon, emptyList())
+
+fun WorkoutPlan.toWorkoutPlanTable(personId: Int) =
+    WorkoutPlanTable(id, name, description, icon, personId)
